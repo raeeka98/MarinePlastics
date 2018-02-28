f6491df9bac6c6241f0c39582f9bf6fcda121a0f
shleewhite <leewhite128@gmail.com>
2018-02-27 21:32:17 -0800

    added styles and header and router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './Header/Header';
 import SurveyBox from './Survey/SurveyBox';
 
 const App = () => (
     <div>
+        <Header />
         <SurveyBox
             url='http://localhost:3001/api/comments'
             pollInterval={2000} 
