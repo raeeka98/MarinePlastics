1f731e07c1fe12f6fe0d81959d1be637484ea458
shleewhite <leewhite128@gmail.com>
2018-02-27 22:32:20 -0800

    set up router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header/Header';
-import Temp from './Temp';
-// import SurveyBox from './Survey/SurveyBox';
+import { BrowserRouter, Route } from 'react-router-dom';
+import SurveyBox from './Survey/SurveyBox';
 
-const App = () => (
-    <div>
-        <Header />
-        <Temp />
-    </div>
-)
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route 
+              exact path='/' 
+              component={ 
+                () => <SurveyBox url='http://localhost:3001/api/comments' pollInterval={2000}/>
+              }
+            />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
 export default App;
