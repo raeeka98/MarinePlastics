1d2a03f2c36b854cd56abca5db4e7404e4a285d2
vincentwu96 <vincentwu96@yahoo.com>
2018-02-27 16:34:29 -0800

    finished first page of form

--- src/SurveyList.js
+++ src/SurveyList.js
@@ -7,16 +7,30 @@ class SurveyList extends Component {
     let commentNodes = this.props.data.map(comment => {
       return (
         <Survey
-          beach={ comment.beach }
+          leader={comment.leader}
           uniqueID={ comment['_id'] }
           onCommentDelete={ this.props.onCommentDelete }
           onCommentUpdate={ this.props.onCommentUpdate }
           key={ comment.id }>
+          {comment.surveyorNames}
+          {comment.contactInfo}
+          {comment.date}
           {comment.reason}
+          {comment.beach}
           {comment.st}
           {comment.lat}
           {comment.lon}
           {comment.slope}
+          {comment.nroName}
+          {comment.nroDist}
+          {comment.nroFlow}
+          {comment.nroOut}
+          {comment.aspect}
+          {comment.weather}
+          {comment.lastTide}
+          {comment.nextTide}
+          {comment.windDir}
+          {comment.majorUse}
         </Survey>
       )
     })
