05abbaaf5d7684599fbc9861f0876106e626a437
shleewhite <leewhite128@gmail.com>
2018-02-28 20:36:07 -0800

    added footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import Header from './Header/Header';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Auth from './Auth';
 
+import Auth from './Auth';
 import SurveyBox from './Survey/SurveyBox';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
 
 class App extends Component {
   constructor() {
@@ -19,19 +20,22 @@ class App extends Component {
     return (
       <div>
         <BrowserRouter>
-          <div>
+          <div className="uk-container uk-container-center">
             <Header 
               auth={this.auth}
             />
-            <Route 
-              exact path='/' 
-              component={ 
-                () => <SurveyBox 
-                        url='http://localhost:3001/api/comments'
-                        pollInterval={2000}
-                      />
-              }
-            />
+            <div className="uk-grid">
+              <Route 
+                exact path='/' 
+                component={ 
+                  () => <SurveyBox 
+                          url='http://localhost:3001/api/comments'
+                          pollInterval={2000}
+                        />
+                }
+              />
+            </div>
+            <Footer />
           </div>
         </BrowserRouter>
       </div>
