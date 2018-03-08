e3de78a6db50572fd7b2c1242b2b82ec01fb2146
guitav <guitapardis@yahoo.com>
2018-03-08 15:07:17 -0800

    Added styling for the pin

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,6 +1,11 @@
-import React, { Component } from 'react';
+import React, { PropTypes, Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
+import Style from './Map.css';
+
+import Pin from './Pin.jsx';
+
+import {K_SIZE} from './PinStyle.js'
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
@@ -12,13 +17,13 @@ class Map extends Component {
 
   render() {
     return (
-       <div style={{height: '1000px', width: '1000px'}}>
+       <div style={{height: '500px', width: '1248px'}}>
       <GoogleMapReact
-    bootstrapURLKeys={{ key: ['AIzaSyC0KMFMCzYY0TZKQSSGyJ7gDW6dfBIDIDA'] }}
-    defaultCenter={this.props.center}
-    defaultZoom={this.props.zoom}
-  >
-    <AnyReactComponent
+        bootstrapURLKeys={{ key: ['AIzaSyC0KMFMCzYY0TZKQSSGyJ7gDW6dfBIDIDA'] }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+      >
+    <Pin
       lat={36.949148}
       lng={-122.060356}
       text={'Testing Location Service'}
