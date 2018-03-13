9480419e38590132e739afd0b0ec9d98bbd25633
guitav <guitapardis@yahoo.com>
2018-03-12 23:49:15 -0700

    Changed the form visually

--- src/Survey/SurveyForm/FormStep2.js
+++ src/Survey/SurveyForm/FormStep2.js
@@ -4,6 +4,59 @@ class FormStep2 extends Component {
   render() {
     return (
       <form>
+    
+  <h2>Survey Area</h2>
+  <label>Name of Beach</label>
+  <input
+    type='text'
+    placeholder='Name of Beach'
+    id='beach'
+    onChange={ this.props.handleInputChange }
+    className='uk-input uk-margin'
+  />
+  <label>Reason for Location Choice</label>
+  <input
+    type='text'
+    placeholder='Reason for Location Choice (ex. Proximity, Problem Spot, etc.)'
+    id='reason'
+    onChange={ this.props.handleInputChange }
+    className='uk-input uk-margin'
+  />
+  <label>Substrate Type</label>
+
+  <input
+    type='text'
+    placeholder='Substrate Type (ex. Sand, Gravel, etc.)'
+    id='st'
+    onChange={ this.props.handleInputChange }
+    className='uk-input uk-margin'
+  />
+
+  <label>GPS Coordinates (Starting Point)</label>
+    <br/>
+  <input
+    type='number'
+    placeholder='Latitude'
+    id='lat'
+    onChange={ this.props.handleInputChange }
+    className='uk-input uk-margin uk-width-1-2@s'
+  />
+  <input
+    type='number'
+    placeholder='Longitude'
+    id='lon'
+    onChange={ this.props.handleInputChange }
+    className='uk-input uk-margin uk-width-1-2@s'
+  />
+
+  <label>Slope</label>
+  <input
+    type='text'
+    placeholder='Slope (ex. Steep, Gradual, Gentle, etc.)'
+    id='slope'
+    onChange={ this.props.handleInputChange }
+    className='uk-input uk-margin'
+  />
         <h2>Survey Area</h2>
         <label>Name of Beach</label>
         <input
@@ -53,59 +106,61 @@ class FormStep2 extends Component {
           className='uk-input uk-margin'
         />
         <label>Nearest River Output</label>
-        <input
-          type='text'
-          placeholder='Name'
-          id='nroName'
-          onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin'
-        />
-        <input
-          type='number'
-          placeholder='Distance (m)'
-          id='nroDist'
-          onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin'
-        />
-        <label>Aspect</label>
-        <input
-          type='text'
-          placeholder='(Compass direction facing water in degrees, perpendicular to spine)'
-          id='aspect'
-          onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin'
-        />
-        <label>Tide Information</label>
-        <input
-          type='text'
-          placeholder='Last Tide / Height'
-          id='lastTide'
-          onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin'
-        />
-        <input
-          type='text'
-          placeholder='Next Tide / Height'
-          id='nextTide'
-          onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin'
-        />
-        <label>Wind Direction</label>
-        <input
-          type='text'
-          placeholder='Wind Speed/Direction'
-          id='windDir'
-          onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin'
-        />
-        <label>Major Usage</label>
-        <input
-          type='text'
-          placeholder='(ex. Recreational, Commercial, Remote/Unused, Private, etc.)'
-          id='majorUse'
-          onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin'
-        />
+    <br/>
+  <input
+    type='text'
+    placeholder='Name'
+    id='nroName'
+    onChange={ this.props.handleInputChange }
+    className='uk-input uk-margin  uk-width-1-2@s'
+  />
+  <input
+    type='number'
+    placeholder='Distance (m)'
+    id='nroDist'
+    onChange={ this.props.handleInputChange }
+    className='uk-input uk-margin  uk-width-1-2@s'
+  />
+  <label>Aspect</label>
+  <input
+    type='text'
+    placeholder='(Compass direction facing water in degrees, perpendicular to spine)'
+    id='aspect'
+    onChange={ this.props.handleInputChange }
+    className='uk-input uk-margin'
+  />
+  <label>Tide Information</label>
+  <br/>
+  <input
+    type='text'
+    placeholder='Last Tide / Height'
+    id='lastTide'
+    onChange={ this.props.handleInputChange }
+    className='uk-input uk-margin  uk-width-1-2@s'
+  />
+  <input
+    type='text'
+    placeholder='Next Tide / Height'
+    id='nextTide'
+    onChange={ this.props.handleInputChange }
+    className='uk-input uk-margin  uk-width-1-2@s'
+  />
+  <label>Wind Direction</label>
+  <input
+    type='text'
+    placeholder='Wind Speed/Direction'
+    id='windDir'
+    onChange={ this.props.handleInputChange }
+    className='uk-input uk-margin'
+  />
+  <label>Major Usage</label>
+  <input
+    type='text'
+    placeholder='(ex. Recreational, Commercial, Remote/Unused, Private, etc.)'
+    id='majorUse'
+    onChange={ this.props.handleInputChange }
+    className='uk-input uk-margin'
+  />
       </form>
     )
   }
--- src/Survey/SurveyForm/FormStep3.js
+++ src/Survey/SurveyForm/FormStep3.js
@@ -5,290 +5,292 @@ class FormStep3 extends Component {
     return(
       <form className="uk-form-horizontal">
         <h2>Surface Rib Scan</h2>
-        <label>Cig Butts </label>
-        <input 
+        <label>Cig Butts </label> <br/>
+        <br/>
+        <input
           type='number'
           placeholder='fresh'
           id='FreshCig'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
         <input
           type='number'
           placeholder='weathered'
           id='WeatheredCig'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
-        <label>Fishing Line </label>
+        <label>Fishing Line </label> <br/>
+        <br/>
         <input
           type='number'
           placeholder='fresh'
           id='FreshFline'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
         <input
           type='number'
           placeholder='weathered'
           id='WeatheredFline'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
-        <label>Glass </label>
+        <label>Glass </label> <br/> <br/>
         <input
           type='number'
           placeholder='fresh'
           id='FreshGlass'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
         <input
           type='number'
           placeholder='weathered'
           id='WeatheredGlass'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
-        <label>Paper </label>
+        <label>Paper </label> <br/>
         <input
           type='number'
           placeholder='fresh'
           id='FreshPaper'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
         <input
           type='number'
           placeholder='weathered'
           id='WeatheredPaper'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
-        <label>Filmed Plastic </label>
+        <label>Filmed Plastic </label> <br/>
         <input
           type='number'
           placeholder='fresh'
           id='FreshFplastic'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
         <input
           type='number'
           placeholder='weathered'
           id='WeatheredFplastic'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
-        <label>Misc. Plastics </label>
+        <label>Misc. Plastics </label> <br/>
         <input
           type='number'
           placeholder='fresh'
           id='FreshMiscPlastic'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
         <input
           type='number'
           placeholder='weathered'
           id='WeatheredMiscPlastic'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
-        <label>Plastic Bottle </label>
+        <label>Plastic Bottle </label> <br/>
         <input
           type='number'
           placeholder='fresh'
           id='FreshPlasticBottle'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
         <input
           type='number'
           placeholder='weathered'
           id='WeatheredPlasticBottle'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
-        <label>Plastic Cap </label>
+        <label>Plastic Cap </label> <br/>
         <input
           type='number'
           placeholder='fresh'
           id='FreshPlasticCap'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
         <input
           type='number'
           placeholder='weathered'
           id='WeatheredPlasticCap'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
-        <label>Styrofoam </label>
+        <label>Styrofoam </label> <br/>
         <input
           type='number'
           placeholder='fresh'
           id='FreshStyrofoam'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
         <input
           type='number'
           placeholder='weathered'
           id='WeatheredStyrofoam'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
-        <label>Wood </label>
+        <label>Wood </label> <br/>
         <input
           type='number'
           placeholder='fresh'
           id='FreshWood'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
         <input
           type='number'
           placeholder='weathered'
           id='WeatheredWood'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
-        <label>Urethane Foam </label>
+        <label>Urethane Foam </label> <br/>
         <input
           type='number'
           placeholder='fresh'
           id='FreshUrethaneFoam'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
         <input
           type='number'
           placeholder='weathered'
           id='WeatheredUrethaneFoam'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
-        <label>Plastic Cup </label>
+        <label>Plastic Cup </label> <br/>
         <input
           type='number'
           placeholder='fresh'
           id='FreshPlasticCup'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
         <input
           type='number'
           placeholder='weathered'
           id='WeatheredPlasticCup'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
-        <label>Plastic Straw </label>
+        <label>Plastic Straw </label> <br/>
         <input
           type='number'
           placeholder='fresh'
           id='FreshPlasticStraw'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
         <input
           type='number'
           placeholder='weathered'
           id='WeatheredPlasticStraw'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
-        <label>Cotton/Cloth </label>
+        <label>Cotton/Cloth </label> <br/>
         <input
           type='number'
           placeholder='fresh'
           id='FreshCottonCloth'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
         <input
           type='number'
           placeholder='weathered'
           id='WeatheredCottonCloth'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
-        <label>Polypropylene Rope </label>
+        <label>Polypropylene Rope </label> <br/>
         <input
           type='number'
           placeholder='fresh'
           id='FreshPolyRope'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
         <input
           type='number'
           placeholder='weathered'
           id='WeatheredPolyRope'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
-        <label>Aluminum Can </label>
+        <label>Aluminum Can </label> <br/>
         <input
           type='number'
           placeholder='fresh'
           id='FreshAlumCan'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
         <input
           type='number'
           placeholder='weathered'
           id='WeatheredAlumCan'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
-        <label>Hygiene Items </label>
+        <label>Hygiene Items </label> <br/>
         <input
           type='number'
           placeholder='fresh'
           id='FreshHygItems'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
         <input
           type='number'
           placeholder='weathered'
           id='WeatheredHygItems'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
-        <label>Metal </label>
+        <label>Metal </label> <br/>
         <input
           type='number'
           placeholder='fresh'
           id='FreshMetal'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
         <input
           type='number'
           placeholder='weathered'
           id='WeatheredMetal'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
-        <label>Tile/Brick </label>
+        <label>Tile/Brick </label> <br/>
         <input
           type='number'
           placeholder='fresh'
           id='FreshTileBrick'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
         <input
           type='number'
           placeholder='weathered'
           id='WeatheredTileBrick'
           onChange={ this.props.handleInputChange }
-          className='uk-input uk-margin srs'
+          className='uk-input uk-margin srs uk-width-1-2@s'
         />
     </form>
     );
