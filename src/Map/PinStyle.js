9d9f2d71bc2f2c19f1ca3c03a1fea77e3aede223
guitav <guitapardis@yahoo.com>
2018-03-12 23:50:59 -0700

    visually changed form and map

--- src/Map/PinStyle.js
+++ src/Map/PinStyle.js
@@ -3,8 +3,9 @@ const K_SIZE = 30;
 const greatPlaceStyle = {
   width: K_SIZE,
   height: K_SIZE,
-  borderRadius: '50% 50% 50% 0',
-  backgroundColor: '#00cae9',
+  borderRadius: '50% 50% 50% 50%',
+  backgroundColor: '#ce5648',
+  color: '#FFFFFF',
   position: 'absolute',
   Left: K_SIZE,
   Top: K_SIZE,
@@ -14,8 +15,8 @@ const greatPlaceStyle = {
 
 const greatPlaceStyleHover = {
   ...greatPlaceStyle,
-  border: '3px solid #3f51b5',
-  color: '#f44336',
+  border: '3px solid #77312a',
+  color: '#FFFFFF',
   cursor: 'pointer'
 };
 
