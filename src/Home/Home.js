572debdee0630d22540c2358c06dd7b03f4747e3
shleewhite <leewhite128@gmail.com>
2018-05-23 15:15:36 -0700

    search by location name works

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import { locationSort } from '../_helpers/SortHelper';
-import { locationFilter } from '../_helpers/SortHelper';
+import { locationSort, locationFind } from '../_helpers/SortHelper';
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = { data: [] };
+    this.state = { data: [], };
     this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
     this.pollInterval = null;
     this.url = 'https://marineplasticsdb.herokuapp.com/api/comments';
   }
@@ -20,8 +20,17 @@ class Home extends Component {
         res.data.sort((a, b) => {
           return new Date(b.date).getTime() - new Date(a.date).getTime();
         });
-        this.setState({ data: res.data });
+        // sorts data into locations 
+        const data = locationSort(res.data);
+        this.setState({ data });
       });
+  }
+
+  handleSearch(e) {
+    if (e.target.value) {
+      const result = locationFind(this.state.data, e.target.value);
+      this.setState({ data: result });
+    }
   }
 
   // once the component is on the page, checks the server for comments every 2000 milliseconds? some sort of interval
@@ -39,10 +48,8 @@ class Home extends Component {
   }
 
   render() {
-    // locations is an array of sorted server entries based on location
-    const locations = locationSort(this.state.data);
     // returns HTML for every entry in the sorted array of locations
-    let locationNodes = locations.map((location, i) => {
+    let locationNodes = this.state.data.map((location, i) => {
       let path = location.name.replace(/\s/g, '');
       return (
         <div className="uk-card uk-card-default uk-card-body uk-margin" key={i}>
@@ -54,15 +61,23 @@ class Home extends Component {
             }}>
               { location.name }
             </Link>
+            <span className="uk-text-muted uk-text-small uk-position-center-right uk-padding">
+              { location.entries.length } Entr{location.entries.length > 1 ? 'ies': 'y'}
+            </span>
           </h3>
-          <p>{ location.entries.length } Entries</p>
         </div>
       );
     });
     // returns a list with all the sorted locations
     return (
       <form className="uk-search uk-search-default uk-width-1-2 uk-align-center">
-        <input className="uk-search-input" id="searchBar" type="search" onKeyPress={ locationFilter({ locations }) } placeholder="Search..."/>
+        <input
+          className="uk-search-input uk-margin uk-text-large uk-padding uk-margin-large-top"
+          id="searchBar"
+          type="search"
+          onKeyPress={ this.handleSearch } 
+          placeholder="Search..."
+        />
         <div id="locations">
           { locationNodes }
         </div>
