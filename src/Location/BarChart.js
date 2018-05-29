875703ac54c7d2a0eff7b3ac48b9bbe8357974eb
Michael  Pluguez <michaelpluguez@dhcp-169-233-167-177.cruznet.ucsc.edu>
2018-05-29 15:15:15 -0700

    Starting D3 barchart

---
+++ src/Location/BarChart.js
@@ -0,0 +1,41 @@
+// src/Chart/Chart.jsx
+
+import React, { Component } from 'react'
+import { scaleBand, scaleLinear } from 'd3-scale'
+
+//import data from '../../data'
+
+export default class Chart extends Component {
+  constructor() {
+    super()
+    this.xScale = scaleBand()
+    this.yScale = scaleLinear()
+  }
+
+  render() {
+    const margins = { top: 50, right: 20, bottom: 100, left: 60 },
+    const svgDimensions = { width: 800, height: 500 }
+
+    //const maxValue = Math.max(...data.map(d => d.value))
+    
+    // scaleBand type
+    const xScale = this.xScale
+      .padding(0.5)
+      // scaleBand domain should be an array of specific values
+      // in our case, we want to use movie titles
+      .domain(data.map(d => d.title))
+      .range([margins.left, svgDimensions.width - margins.right])
+  
+      // scaleLinear type
+      const yScale = this.yScale
+      // scaleLinear domain required at least two values, min and max       
+      .domain([0, 10]) // 0 to maxValue later on
+      .range([svgDimensions.height - margins.bottom, margins.top])
+
+    return (
+      <svg width={svgDimensions.width} height={svgDimensions.height}>
+         // Bars and Axis comes here
+      </svg>
+    )
+  }
+}
--- src/Location/Location.js
+++ src/Location/Location.js
@@ -5,6 +5,8 @@ import GoogleMapReact from 'google-map-react';
 // to get the pin styles
 import '../Map/Map.css';
 
+import '../Location/BarChart.js';
+
 class Location extends Component {
   constructor(props) {
     super(props);
@@ -13,6 +15,16 @@ class Location extends Component {
     this.state = {
       data: this.props.location.state.data || {},
     }
+    var svg = d3.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom;
+  
+    var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+    y = d3.scaleLinear().rangeRound([height, 0]);
+  
+    var g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
   }
 
   render() {
@@ -55,6 +67,12 @@ class Location extends Component {
             </div>
           </div>
           <div>
+            <div className="uk-card uk-card-default uk-card-body">
+              <h3 className="uk-card-title">Survey Entries</h3>
+              <ul>
+                { entries }
+              </ul>
+            </div><br />
             <div className="uk-card uk-card-default uk-card-body">
               <h3 className="uk-card-title">Survey Entries</h3>
               <ul>
