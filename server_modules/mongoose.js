cf6b6dd7bf1886c531f23cf3f18a360266c8d7e8
Noel Gomez <nollgmz4@gmail.com>
2019-02-05 22:49:11 -0800

    First revision of database model

--- server_modules/mongoose.js
+++ server_modules/mongoose.js
@@ -8,16 +8,16 @@ db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 let Schema = mongoose.Schema;
 
 var dataSchema = new Schema({
-    name: String,
+    name: { type: String, required: true },
     fresh: Number,
     weathered: Number,
-}, { versionKey: false });
+}, { versionKey: false, _id: false });
 
 var tideSchema = new Schema({
     type: String,
     time: String,
     height: Number,
-}, { versionKey: false });
+}, { versionKey: false, _id: false });
 
 //create new instance of the mongoose.schema. the schema takes an object that shows
 //the shape of your database entries.
@@ -31,7 +31,11 @@ var CommentsSchema = new Schema({
         required: true
     },
     org: String,
-    input_date: String,
+    input_date: {
+        type: String,
+        required: true,
+        unique: true
+    },
     date: String,
     beach: String,
     reason: String,
@@ -54,6 +58,68 @@ var CommentsSchema = new Schema({
     ASData: [dataSchema],
     ASTotal: Number,
 }, { versionKey: false });
+
+let entrySchema = new Schema({
+    user: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    org: {
+        type: String,
+        required: true
+    },
+    reason: {
+        type: String,
+        required: true
+    },
+    st: String,
+    slope: String,
+    nroName: String,
+    nroDist: Number,
+    aspect: String,
+    lastTide: tideSchema,
+    nextTide: tideSchema,
+    windDir: String,
+    windSpeed: Number,
+    majorUse: String,
+    weight: Number,
+    NumberOfPeople: Number,
+    SRSData: [dataSchema],
+    ASData: [dataSchema]
+}, { versionKey: false })
+
+let entryDate = new Schema({
+    date: {
+        type: Date,
+        required: true,
+        unique: true
+    },
+    entries: [{ type: Schema.Types.ObjectId, ref: 'Entries' }]
+})
+
+let beachSchema = new Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    lat: {
+        type: Number,
+        required: true
+    },
+    lon: {
+        type: Number,
+        required: true
+    },
+    entryDates: [entryDate]
+
+}, { versionKey: false });
+const beachModel = mongoose.model('Beaches',beachSchema);
+const entryModel = mongoose.model('Entries',entrySchema);
 
 const commentModel = mongoose.model('Comment', CommentsSchema);
 
