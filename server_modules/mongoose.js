7f33d998f6edc4b2982f2df07d6cea3736003ee9
Noel Gomez <nollgmz4@gmail.com>
2019-02-08 22:19:16 -0800

    new helper functions for database modification

--- server_modules/mongoose.js
+++ server_modules/mongoose.js
@@ -109,7 +109,8 @@ let beachSchema = new Schema({
     name: {
         type: String,
         unique: true,
-        required: true
+        required: true,
+        index: true
     },
     lat: {
         type: Number,
@@ -119,18 +120,18 @@ let beachSchema = new Schema({
         type: Number,
         required: true
     },
-    entryDates: [{
-        _id: false,
-        date: {
-            type: Date,
-            required: true,
-            unique: true
-        },
-        entries: [{
+    entries: {
+        type: Map,
+        of: {
             type: Schema.Types.ObjectId,
             ref: 'Entries'
-        }]
-    }]
+        }
+    },
+    numOfEntries: {
+        type: Number,
+        default: 0,
+        min: 0
+    }
 }, { versionKey: false });
 
 
@@ -139,24 +140,12 @@ const entryModel = mongoose.model('Entries', entrySchema);
 
 const commentModel = mongoose.model('Comment', CommentsSchema);
 
-let entry = new entryModel({
-    user: 'Noll',
-    email: 'Test@mail.com',
-    org: 'UCSC',
-    reason: 'To test',
-    SRSData: {
-        dirt: {}
-    }
-});
-let beach = new beachModel({
-    name: 'test beach',
-    lat: 123,
-    lon: 456,
-    entryDates: [{
-        date: new Date(),
-        entries: [entry._id]
-    }]
-})
+Date.prototype.toUTCDateString = function() {
+    this.setUTCHours(0, 0, 0, 0);
+    return `${this.getUTCFullYear()}-${this.getUTCMonth()+1}-${this.getUTCDate()} `;
+};
+
+// beach.entries.set(`${new Date().toUTCDateString()}`, entry._id);
 
 // entry.save()
 //     .then(entry => {
@@ -164,20 +153,153 @@ let beach = new beachModel({
 //         return beach.save()
 //             .then(beach => {
 //                 console.log(beach);
-//                 console.log(beach.entryDates);
 //             })
-//     });
+//     })
+//     .then(() => {
+//         beachModel.findById(beach._id)
+//             .populate('entries')
+//             .exec()
+//             .then(beach => {
+//                 console.log(beach);
+//                 console.log(beach.entries);
 
-beachModel.findById("5c5be6645b790130b48ff889")
-    .populate('entryDates.entries')
-    .exec()
-    .then(beach => {
-        console.log(beach);
-        console.log(beach.entryDates[0]);
+//             });
+//     })
+let beach = {
+    name: 'test beach',
+    lat: 123,
+    lon: 456,
+    entries: {}
+}
 
-        console.log(beach.entryDates[0].entries[0]);
+let entry = {
+    user: 'Noll',
+    email: 'Test@mail.com',
+    org: 'UCSC',
+    reason: 'To test',
+    SRSData: {
+        dirt: {}
+    }
+}
 
-    })
+async function test (beach, entry) {
+    let beachID, entryID;
+    try {
+        beachID = await createBeach(beach);
+        entryID = await createEntry(entry);
+        let date = new Date().setHours(0, 0, 0, 0);
+        let newBeach = await AddEntryToBeach(beachID, date, entryID);
+        console.log(newBeach);
+
+        console.log(`${beachID} ${entryID}`);
+    } catch (err) {
+        console.log(err);
+    }
+}
+async function test1 (entry) {
+    let entryID;
+    try {
+        entryID = await createEntry(entry);
+        let date = new Date().getTime();
+        let newBeach = await AddEntryToBeach("5c5e5a298670b02e1ba700eb", date, entryID);
+        console.log(newBeach);
+    } catch (err) {
+        console.log(err);
+    }
+}
+// test(beach, entry)
+// test1(entry)
+// deleteEntry("5c5e5a298670b02e1ba700eb", 1549689034671, "5c5e60b5cd674a2e8d69a697");
+
+// createBeach(beach).then(() => createEntry(entry)).then(beachid => {
+//     console.log(beachid);
+//     return AddEntry(beachid, new Date().getTime(), );
+// })
+
+async function AddEntryToBeach (beachID, epochdate, newEntryID) {
+    let key = `entries.${epochdate}`;
+    let update = {
+        $set: {
+            [key]: newEntryID
+        },
+        $inc: { numOfEntries: 1 }
+    };
+    try {
+        let newBeach = await beachModel.findByIdAndUpdate(beachID, update, { new: true, }).exec();
+        return newBeach;
+    } catch (err) {
+        console.log(err);
+        throw new Error('Error while saving to beach');
+    }
+}
+
+async function deleteEntry (beachID, epochdate, entryID) {
+    let key = `entries.${epochdate}`
+    let update = {
+        $unset: {
+            [key]: 1
+        },
+        $inc: { numOfEntries: -1 }
+    };
+    try {
+        let removeFromBeach = await beachModel.findByIdAndUpdate(beachID, update, { new: true }).exec();
+        console.log(removeFromBeach);
+
+        let removeEntry = entryModel.findByIdAndDelete(entryID).exec();
+        const res = await Promise.all([removeFromBeach, removeEntry]);
+        return res;
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error while deleting entry');
+    }
+}
+
+async function createEntry (survey) {
+    let entry = new entryModel();
+    for (const entryName in survey) {
+        const data = survey[entryName];
+        entry[entryName] = data;
+    }
+    try {
+        let entryRt = await entry.save();
+        return entryRt._id;
+    } catch (err) {
+        console.log(err);
+        throw new Error('Error while saving');
+    }
+}
+
+async function createBeach (beachData) {
+    let location = new beachModel();
+    for (const key in beachData) {
+        const data = beachData[key];
+        location[key] = data;
+    }
+    try {
+        let beachRt = await location.save();
+        return beachRt._id;
+    } catch (err) {
+        console.log(err);
+        throw new Error('Error in saving beach');
+
+    }
+}
+
+deleteBeach("5c5e5a298670b02e1ba700eb");
+
+async function deleteBeach (beachID) {
+    try {
+        let removedBeach = await beachModel.findByIdAndDelete(beachID).exec();
+        let entries = [...removedBeach.entries.values()];
+        await entryModel.deleteMany({ _id: { $in: entries } });
+
+    } catch (err) {
+        console.log(err);
+        throw new Error('Error in deleting beach');
+    }
+}
+
+
 
 
 
