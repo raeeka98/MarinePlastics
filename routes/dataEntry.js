9673faa5dbe216936375e5cd6ccaf47ce442da82
Noel Gomez <nollgmz4@gmail.com>
2019-02-11 23:07:47 -0800

    finished delete survey route

--- routes/dataEntry.js
+++ routes/dataEntry.js
@@ -12,66 +12,39 @@ let asyncHandler = fn =>
 router.route('/')
     //get all beaches
     .get(asyncHandler(async (req, res) => {
-        beaches.getAll()
-            .then(beaches => {
-                res.json(beaches);
-            })
-            .catch(err => { throw new Error(err) });
+        let beaches = await beaches.getAll();
+        res.json(beaches);
     }))
     //delete a beach
     .delete(asyncHandler(async (req, res) => {
         let { bID } = req.body;
-        beaches.remove(bID)
-            .then(() => {
-                res.json({ res: "Successfully deleted beach" })
-            })
-            .catch(err => {
-                console.error(err);
-                res.json({ res: "Failed with error" })
-            })
+        await beaches.remove(bID);
+        res.json({ res: "Successfully deleted beach" });
     }));
 
 router.route('/:beachID')
     .get(asyncHandler(async (req, res) => {
-        beaches.getSurveys(req.params.beachID)
-            .then(surveys => {
-                res.json(surveys);
-            })
-    }))
+        let surveys = beaches.getSurveys(req.params.beachID);
+        res.json(surveys)
+    }));
 
 router.route('/surveys/:surveyID')
     //get a specific entry
     .get(asyncHandler(async (req, res) => {
-        surveys.get(req.params.surveyID)
-            .then(survey => res.json(survey));
+        let survey = await surveys.get(req.params.surveyID);
+        res.json(survey);
     }))
     //find a specific entry and edit it
     .put(asyncHandler(async (req, res) => {
-        surveys.findById(req.params.entryid, function(err, entryData) {
-            if (err) res.send(err);
-            //setting the new beach and reason to whatever was changed. If nothing was changed
-            // we will not alter the field.
-            let newEntryData = res.body;
-            for (const entry in newEntryData) {
-                const data = newEntryData[entry];
-                entryData[entry] = data;
-            }
-
-            //save comment
-            entryData.save(function(err) {
-                if (err)
-                    res.send(err);
-                res.json({ message: 'Entry has been updated' });
-            });
-        });
+        let { oldSurvey, newSurvey } = req.body;
+        let updatedSurvey = await surveys.update(req.params.surveyID, newSurvey, oldSurvey);
+        res.json(updatedSurvey);
     }))
     //delete an entry
     .delete(asyncHandler(async (req, res) => {
-        surveys.remove({ _id: req.params.entryid }, function(err, entry) {
-            if (err)
-                res.send(err);
-            res.json({ message: 'Comment has been deleted' })
-        })
+        let { bID, dateOfSub } = req.body;
+        let res = await surveys.remove(bID, req.params.surveyID, dateOfSub);
+        res.json({ message: 'Comment has been deleted' })
     }));
 
 module.exports = { router };
