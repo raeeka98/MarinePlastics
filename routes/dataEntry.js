a8c6ac519ef2f4f2fa22fc5a9d9449d3d5316ca5
Noel Gomez <nollgmz4@gmail.com>
2019-02-11 17:13:38 -0800

    added async handler

--- routes/dataEntry.js
+++ routes/dataEntry.js
@@ -1,37 +1,53 @@
 let { beaches, surveys } = require('../server_modules/mongoose');
 let router = require('express').Router();
 
+/**
+ * 
+ * @param {Promise} fn 
+ */
+let asyncHandler = fn =>
+    (req, res, next) => {
+        Promise.resolve(fn(req, res, next)).catch(next);
+    }
 router.route('/')
     //get all beaches
-    .get((req, res) => {
-        beaches.getAllBeaches()
+    .get(asyncHandler(async (req, res) => {
+        beaches.getAll()
             .then(beaches => {
                 res.json(beaches);
             })
             .catch(err => { throw new Error(err) });
-    })
+    }))
     //delete a beach
-    .delete((req, res) => {
-        let { beachID } = req.body;
-    });
+    .delete(asyncHandler(async (req, res) => {
+        let { bID } = req.body;
+        beaches.remove(bID)
+            .then(() => {
+                res.json({ res: "Successfully deleted beach" })
+            })
+            .catch(err => {
+                console.error(err);
+                res.json({ res: "Failed with error" })
+            })
+    }));
 
 router.route('/:beachID')
-    .get((req, res) => {
-        beaches.getBeachData(req.params.beachID)
+    .get(asyncHandler(async (req, res) => {
+        beaches.getSurveys(req.params.beachID)
             .then(surveys => {
                 res.json(surveys);
             })
-    })
+    }))
 
 router.route('/surveys/:surveyID')
     //get a specific entry
-    .get((req, res) => {
-        surveys.getSurvey(req.params.surveyID)
+    .get(asyncHandler(async (req, res) => {
+        surveys.get(req.params.surveyID)
             .then(survey => res.json(survey));
-    })
+    }))
     //find a specific entry and edit it
-    .put((req, res) => {
-        entries.findById(req.params.entryid, function(err, entryData) {
+    .put(asyncHandler(async (req, res) => {
+        surveys.findById(req.params.entryid, function(err, entryData) {
             if (err) res.send(err);
             //setting the new beach and reason to whatever was changed. If nothing was changed
             // we will not alter the field.
@@ -48,14 +64,14 @@ router.route('/surveys/:surveyID')
                 res.json({ message: 'Entry has been updated' });
             });
         });
-    })
+    }))
     //delete an entry
-    .delete((req, res) => {
-        entries.remove({ _id: req.params.entryid }, function(err, entry) {
+    .delete(asyncHandler(async (req, res) => {
+        surveys.remove({ _id: req.params.entryid }, function(err, entry) {
             if (err)
                 res.send(err);
             res.json({ message: 'Comment has been deleted' })
         })
-    });
+    }));
 
 module.exports = { router };
