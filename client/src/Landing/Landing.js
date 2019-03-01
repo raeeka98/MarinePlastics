b943d1904d3bed4ed2201dc589f81eb95bcd6285
cassiaa <cassia.artanegara@gmail.com>
2019-02-28 20:28:10 -0800

    edited background pic

--- client/src/Landing/Landing.js
+++ client/src/Landing/Landing.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import background from './beach.jpg';
+import background from './beach-edit.jpg';
+import './landing.css'
 
 // the authentication object is passed through props from App.js
 // which is used to check if the user is currently logged in
@@ -11,7 +12,7 @@ export function LandingPage(props) {
   // force the page to have these styles
   const background_image = {
     backgroundImage: `url(${background})`,
-    height: `100vh`,
+    height: `100vh`
   };
   const textButtonStyles = {
     width: `fit-content`,
@@ -21,26 +22,28 @@ export function LandingPage(props) {
   return (
     <div className="uk-width-1-1">
       
-      <div className="uk-flex uk-flex-middle uk-flex-center uk-background-cover" style={ background_image }>
-        <div className="uk-heading-hero" style={{color: '#ffffff'}}>
-          Welcome to the Marine Plastic Monitor.
-        </div>
-        <div className="uk-background-default uk-flex uk-flex-column uk-padding">
+      <div className="uk-flex uk-flex-middle uk-flex-center uk-flex-column uk-background-blend-overlay uk-background-darken uk-background-cover" style={ background_image }>
+        <div className="uk-flex uk-flex-row uk-padding .uk-animation-slide-bottom">
           { props.auth.isAuthenticated()
             ? null
             : (<button
-                className="uk-button uk-button-primary uk-button-large uk-margin-medium-bottom"
+                className="uk-button uk-button-primary uk-button-large"
                 onClick={ props.auth.login }
               >
                 Sign Up
               </button>)
           }
-          <button className="uk-button uk-button-text" style={ textButtonStyles }>
+          <button className="uk-button uk-button-default uk-button-large" style={ textButtonStyles }>
             <Link className="uk-link-reset" to="/home">
               { props.auth.isAuthenticated() ? 'Continue' : 'Continue as Guest' }
             </Link>
           </button>
         </div>
+        
+        <div className="uk-heading-hero" id="header-text">
+          Welcome to the Marine Plastic Monitor.
+        </div>
+  
       </div>
       { props.isAuth ? <div className="uk-alert-primary" style={{borderRadius:"10px",padding:"5px",display:"inline-block", height:"content",width:"content",position:"absolute",top:"20%",left:"50%",transform:"translateX(-50%)"}}>
           {props.isAuth}
