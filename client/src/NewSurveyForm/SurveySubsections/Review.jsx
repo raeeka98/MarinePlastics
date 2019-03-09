87225b1387a52a8313fc56e41726cbe7b5674550
Frank Kohn <fkohn@ucsc.edu>
2019-03-08 16:30:58 -0800

    update values of survey fields

--- client/src/NewSurveyForm/SurveySubsections/Review.jsx
+++ client/src/NewSurveyForm/SurveySubsections/Review.jsx
@@ -14,19 +14,19 @@ class Review extends Component {
     const d = this.props.data;
     const s = this.props.displayStrings;
     return(
-      <div>
-        <div className="uk-card uk-card-default uk-card-small uk-card-body">
-            <h4 className="uk-card-title">Team Information:</h4>
-            <p>Name: {d.name}</p>
-            <p>Organization Name: {d.orgName}</p>
-            <p>Organization Location: {d.orgLoc}</p>
-            <p>Email Address: {d.email}</p>
-            <p>Clean Up Date: {d.cleanUpDate}</p>
-            <p>Clean Up Start Time: {d.cleanUpTime}</p>
+      <div className="uk-background-muted uk-padding">
+        <div className="uk-card uk-card-default uk-card-body uk-card-hover">
+            <h3 className="uk-card-title">Team Information:</h3>
+            <p>Name: <b>{d.userFirst} {d.userLast}</b></p>
+            <p>Organization Name: <b>{d.orgName}</b></p>
+            <p>Organization Location: <b>{d.orgLoc}</b></p>
+            <p>Email Address: <b>{d.email}</b></p>
+            <p>Clean Up Date: <b>{d.cleanUpDate}</b></p>
+            <p>Clean Up Start Time: <b>{d.cleanUpTime}</b></p>
         </div>
 
-        <div className="uk-card uk-card-default uk-card-small uk-card-body">
-            <h4 className="uk-card-title">Survey Area:</h4>
+        <div className="uk-card uk-card-default uk-card-body uk-card-hover">
+            <h3 className="uk-card-title">Survey Area:</h3>
             <p>Beach Name: {d.name}</p>
             <p>Beach Coordinates: {d.latitude} (lat), {d.longitude} (lon)</p>
             {s.usage.length > 0 &&
@@ -44,11 +44,21 @@ class Review extends Component {
             {d.riverDistance &&
               <p>Nearest River Output Distance: {d.riverDistance}</p>
             }
-            {
 
+            {d.tideTypeB && d.tideTimeB && d.tideHeightB &&
+              <div>
+                <h4>Tide Before:</h4>
+                <p>Type: {d.tideTypeB} </p>
+                <p>Height: {d.tideHeightB}</p>
+                <p>Time: {d.tideTimeB}</p>
+              </div>
             }
-
-
+            {d.tideTypeA && d.tideTimeA && d.tideHeightA &&
+              <div>
+                <h5>Tide After:</h5>
+                <p>{d.tideTypeA} tide of {d.tideTypeA} ft at{d.tideTimeA}</p>
+              </div>
+            }
             <p>Clean Up Start Time: {d.cleanUpTime}</p>
         </div>
       </div>
