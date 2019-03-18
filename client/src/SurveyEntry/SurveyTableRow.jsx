255a0fc84d42dfe0069a5d8c84390db50b308501
Noel Gomez <nollgmz4@gmail.com>
2019-03-18 05:08:41 -0700

    added editablitity

--- client/src/SurveyEntry/SurveyTableRow.jsx
+++ client/src/SurveyEntry/SurveyTableRow.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SurveyTableRow = props => {
-  return (
-    <tr >
-      <td className={props.edit ? "editable" : null}>
-        {props.edit ? <span className="minusBtn"></span> : null}
-        {props.name}
-      </td>
-      <td>{props.fresh}</td>
-      <td>{props.weathered}</td>
-    </tr>
-  );
+class SurveyTableRow extends Component {
+
+  changeDebris = (e) => {
+    this.props.onChange(this.props.type, this.props.trashID, e.target.name, (e.target.value ? e.target.value : 0));
+  }
+
+  deleteDebris = () => {
+    this.props.onClick(this.props.type, this.props.trashID);
+  }
+
+  render() {
+    return (
+      <tr >
+        <td className={this.props.edit ? "editable" : null}>
+          {this.props.edit ? <span className="minusBtn" onClick={this.deleteDebris}></span> : null}
+          {this.props.name}
+        </td>
+        <td>{this.props.edit ? <input type="number" name="fresh" onChange={this.changeDebris} defaultValue={this.props.fresh} /> : this.props.fresh} </td>
+        <td>{this.props.edit ? <input type="number" name="weathered" onChange={this.changeDebris} defaultValue={this.props.weathered} /> : this.props.weathered}</td>
+      </tr>
+    );
+  }
+
 }
 
 export default SurveyTableRow;
