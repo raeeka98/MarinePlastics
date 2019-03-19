3478d6b85da3bf82f5dd4d33535f7e2ed334f22e
Frank Kohn <fkohn@ucsc.edu>
2019-03-19 12:53:22 -0700

    caps

--- client/src/SurveyEntry/SurveyEntry.jsx
+++ client/src/SurveyEntry/SurveyEntry.jsx
@@ -45,6 +45,9 @@ class SurveyEntry extends Component {
     w: "West",
     nw: "Northwest"
   }
+  toTitleCase(word) {
+      return word.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
+  };
 
   renderOptions() {
     if (this.state.surveyData.SRSDebris
@@ -238,7 +241,6 @@ class SurveyEntry extends Component {
   editBtns = () => {
     return (
       <React.Fragment>
-
         {/* Edit and Delete buttons are disabled if user is not logged in or doesn't own the survey */}
         {this.state.editable ?
           <div className="uk-flex uk-flex-row">
@@ -440,7 +442,7 @@ class SurveyEntry extends Component {
               }
               {
                 this.state.surveyData.slope ?
-                  <p><strong>Beach Slope:</strong> {this.state.surveyData.slope}</p> : null
+                  <p><strong>Beach Slope:</strong> {this.toTitleCase(this.state.surveyData.slope)}</p> : null
               }
               {
                 this.state.surveyData.aspect ?
@@ -492,23 +494,23 @@ class SurveyEntry extends Component {
           <div id="tide-section" style={{ display: 'none' }}>
             <div className="uk-card uk-card-default uk-card-body uk-margin-bottom">
               <h3 className="uk-card-title">Tide Information</h3>
-              <h4>The Last Tide</h4>
+              <h4>Last Tide</h4>
               <div>
                 {
                   this.state.surveyData.lastTide ?
                     (<div>
-                      <p><strong>Type:</strong> {this.state.surveyData.lastTide.type}</p>
+                      <p><strong>Type:</strong> {this.toTitleCase(this.state.surveyData.lastTide.type)}</p>
                       <p><strong>Time:</strong> {this.state.surveyData.lastTide.time}</p>
                       <p><strong>Height:</strong> {this.state.surveyData.lastTide.height}</p>
                     </div>) : null
                 }
               </div>
-              <h4>The Next Tide</h4>
+              <h4>Next Tide</h4>
               <div>
                 {
                   this.state.surveyData.nextTide ?
                     (<div>
-                      <p><strong>Type:</strong> {this.state.surveyData.nextTide.type}</p>
+                      <p><strong>Type:</strong> {this.toTitleCase(this.state.surveyData.nextTide.type)}</p>
                       <p><strong>Time:</strong> {this.state.surveyData.nextTide.time}</p>
                       <p><strong>Height:</strong> {this.state.surveyData.nextTide.height}</p>
                     </div>) : null
