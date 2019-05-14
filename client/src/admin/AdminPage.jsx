409c04d9690a3fd18d84976164aac87add0be897
Noel Gomez <nollgmz4@gmail.com>
2019-05-14 15:59:44 -0700

    admin update

--- client/src/admin/AdminPage.jsx
+++ client/src/admin/AdminPage.jsx
@@ -2,19 +2,67 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './adminPage.css';
 
-class AdminPage extends Component{
+class AdminPage extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
             auth: this.props.auth,
-            currentFoundUser:null
+            currentFoundUser: null,
+            searchEmail: ''
         };
 
     }
 
-    render(){
-        return (<p>hi</p>);
+    emailChange = (e) => {
+        this.setState({ searchEmail: e.target.value });
+    }
+
+    searchEmail = () => {
+        console.log(this.state.auth.getAccessToken());
+        let token = this.state.auth.getAccessToken();
+
+        axios.get("/auth/find", {
+            params: {
+                e: this.state.searchEmail
+            },
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then(res => {
+                console.log(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    button = () => {
+        return (
+            this.state.searchEmail.length > 0 && this.state.searchEmail.includes('@')
+                ? <button className="uk-button uk-button-default uk-button-primary uk-margin-small-top" type="submit" onClick={this.searchEmail}>Search</button>
+                : <button className="uk-button uk-button-default uk-button-primary uk-margin-small-top" type="submit" onClick={this.searchEmail} disabled>Search</button>
+        )
+    }
+
+    render() {
+        // axios.get("/auth/getAdmins", {
+        // headers: {
+        // Authorization: `Bearer ${this.props.auth.getAccessToken()}`
+        // }
+        // })
+        return (
+            <div className=" uk-container uk-container-center">
+                <div className=" searchBlock uk-margin">
+                    <label htmlFor="email">Search for user by email</label>
+                    <div className="emailInput">
+                        <input placeholder="email@example.com" className="uk-input" type="email" name="emailAddr" id="addr" value={this.state.searchEmail} onChange={this.emailChange} />
+                        {this.button()}
+                    </div>
+                </div>
+            </div>
+        );
     }
 
 }
--- routes/auth0.js
+++ routes/auth0.js
@@ -1,35 +1,107 @@
 const axios = require("axios").default;
 const router = require("express").Router();
-const jwtAuthz = require("express-jwt-authz");
-
-const checkScopes = jwtAuthz(['create:admin']);
 
 
+let managAPI = {
+    body: {
+        client_id: "eg6wqMt4RF4wke7rsyK2FC40L9jJUnWb",
+        client_secret: "nWqsoGr_Uab-T4C2K-xW7gvg-Fqf3lgFduj4HD6o7L49eZwzaoShq4NBi4gif4Lm",
+        audience: "https://marine-plastics-coi.auth0.com/api/v2/",
+        grant_type: "client_credentials"
+    },
+    accessToken: {
+        token: null,
+        expiresAt: 0
+    }
+}
 
-module.exports = function(checkJwt) {
+let userAPI = {
+    body: {
+        client_id: "eg6wqMt4RF4wke7rsyK2FC40L9jJUnWb",
+        client_secret: "nWqsoGr_Uab-T4C2K-xW7gvg-Fqf3lgFduj4HD6o7L49eZwzaoShq4NBi4gif4Lm",
+        audience: "https://user-auth.com",
+        grant_type: "client_credentials",
+    },
+    accessToken: {
+        token: null,
+        expiresAt: 0
+    }
+}
 
-    router.get("/find", checkJwt, checkScopes, (req, res) => {
-        console.log(req.user);
-        let reqEmail = req.query.email;
-        axios.get("https://marine-plastics-coi.auth0.com/api/v2/users-by-email", {
-                params: {
-                    email: reqEmail
-                },
-                headers: {
-                    authorization: req.headers['authorization']
-                }
-            })
+function hasPermission (req, res, next) {
+    console.log(req.user);
+
+    let hasPermission = req.user.permissions.includes('create:admin');
+    if (!hasPermission) {
+        return res.status(403).json({ err: "Do not have the right permissions." });
+    }
+    next();
+}
+
+function obtainAccessToken (API) {
+    return new Promise((res, rej) => {
+        let { body: apiBody, accessToken: currtoken } = API === "user" ? userAPI : managAPI;
+        let currentTime = new Date().getTime();
+        if (currtoken.token && currtoken.expiresAt < currentTime) {
+            res(currentToken.token);
+        }
+        let chosenAPI =
+            axios.post("https://marine-plastics-coi.auth0.com/oauth/token", )
             .then(res => {
-                console.log(res.data);
+
+                let tokenData = res.data;
+                console.log(tokenData.access_token);
+                currentToken.token = tokenData.access_token;
+                currentToken.expiresAt = (tokenData.expires_in * 1000) + new Date().getTime();
             })
             .catch(err => {
-                console.log("sth went wrong");
-                console.log(err);
+                console.log(err.response.data);
+
             })
+    })
+}
+
+obtainAccessToken();
+
+
+
+
+module.exports = function(checkJwt, jwtManagement) {
+    router.use(checkJwt, hasPermission);
+
+    router.get("/find", (req, res) => {
+        let reqEmail = req.query.e;
+        console.log(reqEmail);
+        console.log(req.headers['authorization']);
+
+
+
+        // axios.get("https://marine-plastics-coi.auth0.com/api/v2/users-by-email", {
+        //         params: {
+        //             email: reqEmail
+        //         },
+        //         headers: {
+        //             Authorization: req.headers['authorization']
+        //         }
+        //     })
+        //     .then(res => {
+        //         console.log(res.data);
+        //     })
+        //     .catch(err => {
+        //         console.log("sth went wrong");
+        //         console.log(err);
+        //     })
     });
 
+    function serv () {
+        console.log("serv");
 
-    router.post("/setRole", checkJwt, checkScopes, (req, res) => {
+
+    }
+
+
+
+    router.post("/setRole", (req, res) => {
         let { userID } = req.body;
         console.log("new Role");
 
@@ -38,7 +110,7 @@ module.exports = function(checkJwt) {
             }, {
                 headers: {
                     'cache-control': 'no-cache',
-                    authorization: req.headers['authorization'],
+                    Authorization: req.headers['authorization'],
                     'content-type': 'application/json'
                 }
             })
@@ -50,6 +122,21 @@ module.exports = function(checkJwt) {
 
             })
     });
+
+    router.get("/getAdmins", (req, res) => {
+        axios.get("https://marine-plastics-coi.auth0.com/api/v2/roles/rol_TeEKH4d1DDLAbCVT/users", {
+                headers: {
+                    authorization: req.headers['authorization']
+                }
+            })
+            .then(res => {
+                console.log(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+
+            })
+    })
 
     return router;
 }
