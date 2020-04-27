f70e220a2c2eac947c51b2958cb0bd871e25dea2
Sean Gibson <stgibson@ucsc.edu>
2020-04-27 00:27:18 -0700

    Fixed formatting.

--- server_modules/mongooseSchemas.js
+++ server_modules/mongooseSchemas.js
@@ -8,10 +8,10 @@
 var mongoose = require('mongoose');
 const mongoDB = process.env.DB_URL;
 mongoose.connect(mongoDB, {
-    useNewUrlParser: true,
-    autoReconnect: true,
-    reconnectTries: Number.MAX_VALUE,
-    reconnectInterval: 5000
+  useNewUrlParser: true,
+  autoReconnect: true,
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 5000
 });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error: '));
@@ -19,129 +19,129 @@ let Schema = mongoose.Schema;
 
 // includes total number of a type of trash for a survey (fresh and weathered)
 let newDataSchema = new Schema({
-    fresh: { type: Number },
-    weathered: { type: Number }
+  fresh: { type: Number },
+  weathered: { type: Number }
 }, { versionKey: false, _id: false, validateBeforeSave: false });
 
 // defines the substrate options for a survey
 let substrateTypeSchema = new Schema({
-    s: {
-        type: Boolean,
-        alias: "sand",
-        default: undefined
-    },
-    p: {
-        type: Boolean,
-        alias: "pebble",
-        default: undefined
-    },
-    rr: {
-        type: Boolean,
-        alias: "rip_rap",
-        default: undefined
-    },
-    sea: {
-        type: Boolean,
-        alias: "seaweed",
-        default: undefined
-    },
-    other: String
+  s: {
+    type: Boolean,
+    alias: "sand",
+    default: undefined
+  },
+  p: {
+    type: Boolean,
+    alias: "pebble",
+    default: undefined
+  },
+  rr: {
+    type: Boolean,
+    alias: "rip_rap",
+    default: undefined
+  },
+  sea: {
+    type: Boolean,
+    alias: "seaweed",
+    default: undefined
+  },
+  other: String
 }, { versionKey: false, _id: false, validateBeforeSave: false });
 
 // defines the usage options for a survey
 let majorUsageSchema = new Schema({
-    rec: {
-        type: Boolean,
-        default: undefined,
-        alias: "recreational"
-    },
-    com: {
-        type: Boolean,
-        default: undefined,
-        alias: "commercial"
-    },
-    other: String
+  rec: {
+    type: Boolean,
+    default: undefined,
+    alias: "recreational"
+  },
+  com: {
+    type: Boolean,
+    default: undefined,
+    alias: "commercial"
+  },
+  other: String
 }, { versionKey: false, _id: false, validateBeforeSave: false });
 
 // defines location reason options for a survey
 let locationReason = new Schema({
-    prox: {
-        type: Boolean,
-        default: undefined,
-        alias: "proximity"
-    },
-    // no alias because not using an abbreviation
-    debris: {
-        type: Boolean,
-        default: undefined
-    },
-    other: String
+  prox: {
+    type: Boolean,
+    default: undefined,
+    alias: "proximity"
+  },
+  // no alias because not using an abbreviation
+  debris: {
+    type: Boolean,
+    default: undefined
+  },
+  other: String
 }, { versionKey: false, _id: false, validateBeforeSave: false })
 
 // defines description of a low or high tide for a survey
 var tideSchema = new Schema({
-    type: String,
-    time: String,
-    height: Number,
+  type: String,
+  time: String,
+  height: Number,
 }, { versionKey: false, _id: false, validateBeforeSave: false });
 
 // defines a survey, and uses the above schemas
 let surveySchema = new Schema({
-    bID: { type: mongoose.Types.ObjectId, ref: 'Beaches', index: true },
-    user: {
-        f: {
-            type: String,
-            alias: "first"
-        },
-        l: {
-            type: String,
-            alias: "last"
-        },
+  bID: { type: mongoose.Types.ObjectId, ref: 'Beaches', index: true },
+  user: {
+    f: {
+      type: String,
+      alias: "first"
     },
-    userID: String,
-    email: {
-        type: String,
+    l: {
+      type: String,
+      alias: "last"
     },
-    org: {
-        type: String,
-    },
-    reason: locationReason,
-    survDate: {
-        type: Date,
-    },
-    st: substrateTypeSchema,
-    slope: String,
-    weight: Number,
-    cmpsDir: {
-        type: Number,
-        alias: "compassDirection"
-    },
-    lastTide: tideSchema,
-    nextTide: tideSchema,
-    wind: {
-        dir: { type: String },
-        spd: { type: Number }
-    },
-    majorUse: majorUsageSchema,
-    numOfP: {
-        type: Number,
-        alias: "NumberOfPeople"
-    },
-    SRSDebris: {
-        type: Map,
-        of: newDataSchema
-    },
-    ASDebris: {
-        type: Map,
-        of: newDataSchema
-    },
-    MicroDebris: {
-        type: Map,
-        of: newDataSchema
-    },
-    srsDebrisLength: { type: Number, required: true, min: 0 },
-    asDebrisLength: { type: Number, required: true, min: 0 },
-    microDebrisLength: { type: Number, required: true, min: 0 }
+  },
+  userID: String,
+  email: {
+    type: String,
+  },
+  org: {
+    type: String,
+  },
+  reason: locationReason,
+  survDate: {
+    type: Date,
+  },
+  st: substrateTypeSchema,
+  slope: String,
+  weight: Number,
+  cmpsDir: {
+    type: Number,
+    alias: "compassDirection"
+  },
+  lastTide: tideSchema,
+  nextTide: tideSchema,
+  wind: {
+    dir: { type: String },
+    spd: { type: Number }
+  },
+  majorUse: majorUsageSchema,
+  numOfP: {
+    type: Number,
+    alias: "NumberOfPeople"
+  },
+  SRSDebris: {
+    type: Map,
+    of: newDataSchema
+  },
+  ASDebris: {
+    type: Map,
+    of: newDataSchema
+  },
+  MicroDebris: {
+    type: Map,
+    of: newDataSchema
+  },
+  srsDebrisLength: { type: Number, required: true, min: 0 },
+  asDebrisLength: { type: Number, required: true, min: 0 },
+  microDebrisLength: { type: Number, required: true, min: 0 }
 }, { versionKey: false, validateBeforeSave: false });
 
 /**
@@ -152,17 +152,17 @@ let surveySchema = new Schema({
  * @return total amount of trash from the surface rib scan
  */
 surveySchema.methods.getSRSTotal = function(newDebris) {
-    let total = 0;
-    this.SRSDebris.forEach((trashData, trash, map) => {
-        const trashAmount = trashData.fresh + trashData.weathered;
-        total += trashAmount;
-        if (newDebris.hasOwnProperty(trash)) {
-            newDebris[trash] += trashAmount;
-        } else {
-            newDebris[trash] = trashAmount;
-        }
-    });
-    return total;
+  let total = 0;
+  this.SRSDebris.forEach((trashData, trash, map) => {
+    const trashAmount = trashData.fresh + trashData.weathered;
+    total += trashAmount;
+    if (newDebris.hasOwnProperty(trash)) {
+      newDebris[trash] += trashAmount;
+    } else {
+      newDebris[trash] = trashAmount;
+    }
+  });
+  return total;
 }
 
 /**
@@ -173,17 +173,17 @@ surveySchema.methods.getSRSTotal = function(newDebris) {
  * @return total amount of trash from the accumulation survey
  */
 surveySchema.methods.getASTotal = function(newDebris) {
-    let total = 0;
-    this.ASDebris.forEach((trashData, trash, map) => {
-        const trashAmount = trashData.fresh + trashData.weathered;
-        total += trashAmount;
-        if (newDebris.hasOwnProperty(trash)) {
-            newDebris[trash] += trashAmount;
-        } else {
-            newDebris[trash] = trashAmount;
-        }
-    });
-    return total;
+  let total = 0;
+  this.ASDebris.forEach((trashData, trash, map) => {
+    const trashAmount = trashData.fresh + trashData.weathered;
+    total += trashAmount;
+    if (newDebris.hasOwnProperty(trash)) {
+      newDebris[trash] += trashAmount;
+    } else {
+      newDebris[trash] = trashAmount;
+    }
+  });
+  return total;
 };
 
 /**
@@ -192,24 +192,24 @@ surveySchema.methods.getASTotal = function(newDebris) {
  * @return total amount of trash
  */
 surveySchema.methods.getAllDebris = function() {
-    let allDebris = {};
-    this.ASDebris.forEach((trashData, trash) => {
-        const trashAmount = trashData.fresh + trashData.weathered;
-        if (allDebris.hasOwnProperty(trash)) {
-            allDebris[trash] += trashAmount;
-        } else {
-            allDebris[trash] = trashAmount;
-        }
-    });
-    this.SRSDebris.forEach((trashData, trash) => {
-        const trashAmount = trashData.fresh + trashData.weathered;
-        if (allDebris.hasOwnProperty(trash)) {
-            allDebris[trash] += trashAmount;
-        } else {
-            allDebris[trash] = trashAmount;
-        }
-    });
-    return allDebris;
+  let allDebris = {};
+  this.ASDebris.forEach((trashData, trash) => {
+    const trashAmount = trashData.fresh + trashData.weathered;
+    if (allDebris.hasOwnProperty(trash)) {
+      allDebris[trash] += trashAmount;
+    } else {
+      allDebris[trash] = trashAmount;
+    }
+  });
+  this.SRSDebris.forEach((trashData, trash) => {
+    const trashAmount = trashData.fresh + trashData.weathered;
+    if (allDebris.hasOwnProperty(trash)) {
+      allDebris[trash] += trashAmount;
+    } else {
+      allDebris[trash] = trashAmount;
+    }
+  });
+  return allDebris;
 };
 
 /**
@@ -218,134 +218,134 @@ surveySchema.methods.getAllDebris = function() {
  * @return total number of trash as a negative number
  */
 surveySchema.methods.getAllDebrisNeg = function() {
-    let allDebris = {};
-    this.ASDebris.forEach((trashData, trash) => {
-        const trashAmount = trashData.fresh + trashData.weathered;
-        if (allDebris.hasOwnProperty(trash)) {
-            allDebris[trash] -= trashAmount;
-        } else {
-            allDebris[trash] = -trashAmount;
-        }
-    });
-    this.SRSDebris.forEach((trashData, trash) => {
-        const trashAmount = trashData.fresh + trashData.weathered;
-        if (allDebris.hasOwnProperty(trash)) {
-            allDebris[trash] -= trashAmount;
-        } else {
-            allDebris[trash] = -trashAmount;
-        }
-    });
-    return allDebris;
+  let allDebris = {};
+  this.ASDebris.forEach((trashData, trash) => {
+    const trashAmount = trashData.fresh + trashData.weathered;
+    if (allDebris.hasOwnProperty(trash)) {
+      allDebris[trash] -= trashAmount;
+    } else {
+      allDebris[trash] = -trashAmount;
+    }
+  });
+  this.SRSDebris.forEach((trashData, trash) => {
+    const trashAmount = trashData.fresh + trashData.weathered;
+    if (allDebris.hasOwnProperty(trash)) {
+      allDebris[trash] -= trashAmount;
+    } else {
+      allDebris[trash] = -trashAmount;
+    }
+  });
+  return allDebris;
 };
 
 // stores the total trash collected on a certain date, both SRS and AS
 let dayTotalsSchema = new Schema({
-    date: { type: Number, index: true },
-    AST: { type: Number, required: true, default: 0, min: 0 },
-    SRST: { type: Number, required: true, default: 0, min: 0 }
+  date: { type: Number, index: true },
+  AST: { type: Number, required: true, default: 0, min: 0 },
+  SRST: { type: Number, required: true, default: 0, min: 0 }
 }, { versionKey: false, _id: false, validateBeforeSave: false })
 
 // stores total trash collected each month in a year, each number is a month
 let yearTotalsSchema = new Schema({
-    "0": { type: [dayTotalsSchema], default: undefined },
-    "1": { type: [dayTotalsSchema], default: undefined },
-    "2": { type: [dayTotalsSchema], default: undefined },
-    "3": { type: [dayTotalsSchema], default: undefined },
-    "4": { type: [dayTotalsSchema], default: undefined },
-    "5": { type: [dayTotalsSchema], default: undefined },
-    "6": { type: [dayTotalsSchema], default: undefined },
-    "7": { type: [dayTotalsSchema], default: undefined },
-    "8": { type: [dayTotalsSchema], default: undefined },
-    "9": { type: [dayTotalsSchema], default: undefined },
-    "10": { type: [dayTotalsSchema], default: undefined },
-    "11": { type: [dayTotalsSchema], default: undefined },
+  "0": { type: [dayTotalsSchema], default: undefined },
+  "1": { type: [dayTotalsSchema], default: undefined },
+  "2": { type: [dayTotalsSchema], default: undefined },
+  "3": { type: [dayTotalsSchema], default: undefined },
+  "4": { type: [dayTotalsSchema], default: undefined },
+  "5": { type: [dayTotalsSchema], default: undefined },
+  "6": { type: [dayTotalsSchema], default: undefined },
+  "7": { type: [dayTotalsSchema], default: undefined },
+  "8": { type: [dayTotalsSchema], default: undefined },
+  "9": { type: [dayTotalsSchema], default: undefined },
+  "10": { type: [dayTotalsSchema], default: undefined },
+  "11": { type: [dayTotalsSchema], default: undefined },
 }, { versionKey: false, validateBeforeSave: false });
 
 // stores a survey along with the date the data was collected
 let daySurveySchema = new Schema({
-    date: { type: Number, index: true, required: true },
-    survey: { type: String }
+  date: { type: Number, index: true, required: true },
+  survey: { type: String }
 }, { versionKey: false, _id: false, validateBeforeSave: false })
 
 // stores surveys done each month in a year, each number is a month
 let yearSurveySchema = new Schema({
-    "0": { type: [daySurveySchema], default: undefined },
-    "1": { type: [daySurveySchema], default: undefined },
-    "2": { type: [daySurveySchema], default: undefined },
-    "3": { type: [daySurveySchema], default: undefined },
-    "4": { type: [daySurveySchema], default: undefined },
-    "5": { type: [daySurveySchema], default: undefined },
-    "6": { type: [daySurveySchema], default: undefined },
-    "7": { type: [daySurveySchema], default: undefined },
-    "8": { type: [daySurveySchema], default: undefined },
-    "9": { type: [daySurveySchema], default: undefined },
-    "10": { type: [daySurveySchema], default: undefined },
-    "11": { type: [daySurveySchema], default: undefined },
+  "0": { type: [daySurveySchema], default: undefined },
+  "1": { type: [daySurveySchema], default: undefined },
+  "2": { type: [daySurveySchema], default: undefined },
+  "3": { type: [daySurveySchema], default: undefined },
+  "4": { type: [daySurveySchema], default: undefined },
+  "5": { type: [daySurveySchema], default: undefined },
+  "6": { type: [daySurveySchema], default: undefined },
+  "7": { type: [daySurveySchema], default: undefined },
+  "8": { type: [daySurveySchema], default: undefined },
+  "9": { type: [daySurveySchema], default: undefined },
+  "10": { type: [daySurveySchema], default: undefined },
+  "11": { type: [daySurveySchema], default: undefined },
 }, { versionKey: false, validateBeforeSave: false });
 
 // stores data on a beach for auto fill, and for the surveys done on the beach
 let beachSchema = new Schema({
-    n: {
-        type: String,
-        unique: true,
-        required: true,
-        index: true,
-        alias: "name",
+  n: {
+    type: String,
+    unique: true,
+    required: true,
+    index: true,
+    alias: "name",
+  },
+  lat: {
+    type: Number,
+    required: true,
+    min: -85,
+    max: 85
+  },
+  lon: {
+    type: Number,
+    required: true,
+    min: -180,
+    max: 180
+  },
+  lastMod: { type: Date, default: Date.now, index: true },
+  nroName: String,
+  nroDist: { type: Number, min: 0 },
+  numSurvey: { type: Number, min: 0 },
+  surveys: {
+    type: Map,
+    of: { type: mongoose.Types.ObjectId, ref: "YearSurveys" },
+    default: {}
+  },
+  stats: {
+    ttls: {
+      type: Map,
+      of: { type: mongoose.Types.ObjectId, ref: "YearTotals" },
+      default: {},
+      alias: "totals"
     },
-    lat: {
-        type: Number,
-        required: true,
-        min: -85,
-        max: 85
+    TODF: {
+      type: Map,
+      of: { type: Number, default: 0, min: 0 },
+      default: {},
+      alias: "typesOfDebrisFound"
     },
-    lon: {
-        type: Number,
-        required: true,
-        min: -180,
-        max: 180
-    },
-    lastMod: { type: Date, default: Date.now, index: true },
-    nroName: String,
-    nroDist: { type: Number, min: 0 },
-    numSurvey: { type: Number, min: 0 },
-    surveys: {
-        type: Map,
-        of: { type: mongoose.Types.ObjectId, ref: "YearSurveys" },
-        default: {}
-    },
-    stats: {
-        ttls: {
-            type: Map,
-            of: { type: mongoose.Types.ObjectId, ref: "YearTotals" },
-            default: {},
-            alias: "totals"
-        },
-        TODF: {
-            type: Map,
-            of: { type: Number, default: 0, min: 0 },
-            default: {},
-            alias: "typesOfDebrisFound"
-        },
-        lastUp: {
-            type: Date,
-            default: null,
-            alias: "lastUpdated"
-        }
+    lastUp: {
+      type: Date,
+      default: null,
+      alias: "lastUpdated"
     }
+  }
 }, { versionKey: false, validateBeforeSave: false });
 
 // data on a type of trash
 let trashSchema = new Schema({
-    trash_id: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    name: {
-        type: String,
-        unique: true,
-        required: true
-    }
+  trash_id: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  name: {
+    type: String,
+    unique: true,
+    required: true
+  }
 }, { versionKey: false, validateBeforeSave: false });
 
 
@@ -359,4 +359,10 @@ const yearTotalsModel = mongoose.model("YearTotals", yearTotalsSchema);
 
 // const commentModel = mongoose.model('Comment', CommentsSchema);
 
-module.exports = { beachModel, surveyModel, yearSurveyModel, trashModel, yearTotalsModel };
+module.exports = {
+  beachModel,
+  surveyModel,
+  yearSurveyModel,
+  trashModel,
+  yearTotalsModel
+};
