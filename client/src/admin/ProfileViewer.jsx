f107b9344f471718be28802bf9c6d253cc15d4e7
Sean Gibson <stgibson@ucsc.edu>
2020-06-01 21:55:50 -0700

    Added comments and reformatted admin page files. Modified client/src/admin/AdminListChild.jsx, client/src/admin/AdminPage.jsx, and client/src/admin/ProfileViewer.jsx.

--- client/src/admin/ProfileViewer.jsx
+++ client/src/admin/ProfileViewer.jsx
@@ -1,29 +1,49 @@
+/**
+ * ProfileViewer.jsx
+ * Shows full profile of user on admin page when admin searches a user by
+ * email.
+ */
 import React from 'react';
 
+/**
+ * JSX code to show the full profile of a user, if it has been searched.
+ * Otherwise, explains that the current searched user doesn't have a profile.
+ * @param {any} profile
+ * @return JSX code for profile
+ */
 const showProfileInfo = profile => {
-    if (!profile) {
-        return (<div>No profile please search for another</div>);
-    }
-    console.log(profile);
+  if (!profile) {
+    return (<div>No profile please search for another</div>);
+  }
+  console.log(profile);
     
-    let date = new Date(profile.last_login);
-    return (
-        <React.Fragment>
-            <img src={profile.picture} alt="profile" className="profImg uk-border-circle uk-align-center" />
-            <div>Email: <span>{profile.email}</span></div>
-            <div>Full name: <span>{profile.name}</span></div>
-            <div>Last logged In: <span>{date.toString()} </span></div>
-        </React.Fragment>
-    );
+  let date = new Date(profile.last_login);
+  return (
+    <React.Fragment>
+      <img
+        src={profile.picture}
+        alt="profile"
+        className="profImg uk-border-circle uk-align-center"
+      />
+      <div>Email: <span>{profile.email}</span></div>
+      <div>Full name: <span>{profile.name}</span></div>
+      <div>Last logged In: <span>{date.toString()} </span></div>
+    </React.Fragment>
+  );
 }
 
+/**
+ * JSX code to render showProfileInfo(profile)
+ * @param {any} props
+ * @return JSX code
+ */
 const ProfileViewer = props => {
-    let { profile } = props;
-    return (
-        <React.Fragment>
-            {showProfileInfo(profile)}
-        </React.Fragment>
-    );
+  let { profile } = props;
+  return (
+    <React.Fragment>
+      {showProfileInfo(profile)}
+    </React.Fragment>
+  );
 }
 
 export default ProfileViewer;
