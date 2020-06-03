48367c8b09b28825a54ad6ee915f66d2fc5c73ef
Sean Gibson <stgibson@ucsc.edu>
2020-06-02 17:43:52 -0700

    Added comments and reformatted code for 4 files. Modified client/src/Auth.js, client/src/index.js, routes/auth0.js, and server.js.

--- routes/auth0.js
+++ routes/auth0.js
@@ -1,166 +1,191 @@
+/**
+ * auth0.js
+ * Sets up calls that can be made to access information on and modify users
+ * permissions.
+ */
 const axios = require("axios").default;
 const router = require("express").Router();
 
-
+// API for managing website
 let managAPI = {
-    body: {
-        client_id: "eg6wqMt4RF4wke7rsyK2FC40L9jJUnWb",
-        client_secret: "nWqsoGr_Uab-T4C2K-xW7gvg-Fqf3lgFduj4HD6o7L49eZwzaoShq4NBi4gif4Lm",
-        audience: "https://marine-plastics-coi.auth0.com/api/v2/",
-        grant_type: "client_credentials"
-    },
-    accessToken: {
-        token: null,
-        expiresAt: 0
-    }
+  body: {
+    client_id: "eg6wqMt4RF4wke7rsyK2FC40L9jJUnWb",
+    client_secret:
+      "nWqsoGr_Uab-T4C2K-xW7gvg-Fqf3lgFduj4HD6o7L49eZwzaoShq4NBi4gif4Lm",
+    audience: "https://marine-plastics-coi.auth0.com/api/v2/",
+    grant_type: "client_credentials"
+  },
+  accessToken: {
+    token: null,
+    expiresAt: 0
+  }
 }
 
-
+// API for using website
 let userAPI = {
-    body: {
-        client_id: "eg6wqMt4RF4wke7rsyK2FC40L9jJUnWb",
-        client_secret: "nWqsoGr_Uab-T4C2K-xW7gvg-Fqf3lgFduj4HD6o7L49eZwzaoShq4NBi4gif4Lm",
-        audience: "https://user-auth.com",
-        grant_type: "client_credentials",
-    },
-    accessToken: {
-        token: null,
-        expiresAt: 0
-    }
+  body: {
+    client_id: "eg6wqMt4RF4wke7rsyK2FC40L9jJUnWb",
+    client_secret:
+      "nWqsoGr_Uab-T4C2K-xW7gvg-Fqf3lgFduj4HD6o7L49eZwzaoShq4NBi4gif4Lm",
+    audience: "https://user-auth.com",
+    grant_type: "client_credentials",
+  },
+  accessToken: {
+    token: null,
+    expiresAt: 0
+  }
 }
 
-function hasPermission (req, res, next) {
-    //check if they have correct permission to access this route
-    let hasPermission = req.user.permissions.includes('create:admin');
-    if (!hasPermission) {
-        return res.status(403).json({ err: "Do not have the right permissions." });
-    }
-    next();
+/**
+ * Checks if they have correct permission to access this route.
+ * @params {any} req, {any} res, {any} next
+ * @return error message only if user does not have permission
+ */
+function hasPermission(req, res, next) {
+  let hasPermission = req.user.permissions.includes('create:admin');
+  if (!hasPermission) {
+    return res.status(403).json({ err: "Do not have the right permissions." });
+  }
+  next();
 }
 
+/**
+ * Gets access token to use API, then stores it for later use.
+ * @param {any} API
+ * @return access token
+ */
 async function obtainAccessToken (API) {
-    // get access token to use API then store it for later use
-    let { body: apiBody, accessToken: currtoken } = API === "user" ? userAPI : managAPI;
-    let currentTime = new Date().getTime();
-    if (currtoken.token && currtoken.expiresAt < currentTime) {
-        return currtoken.token;
-    }
-    let res = await axios.post("https://marine-plastics-coi.auth0.com/oauth/token", apiBody)
-    let tokenData = res.data;
-    currtoken.token = tokenData.access_token;
-    currtoken.expiresAt = (tokenData.expires_in * 1000) + new Date().getTime();
+  let { body: apiBody, accessToken: currtoken } = API === "user" ? userAPI :
+    managAPI;
+  let currentTime = new Date().getTime();
+  if (currtoken.token && currtoken.expiresAt < currentTime) {
     return currtoken.token;
+  }
+  let res = await
+    axios.post("https://marine-plastics-coi.auth0.com/oauth/token", apiBody)
+  let tokenData = res.data;
+  currtoken.token = tokenData.access_token;
+  currtoken.expiresAt = (tokenData.expires_in * 1000) + new Date().getTime();
+  return currtoken.token;
 }
 
+/**
+ * Makes a promise to call fn.
+ * @param {any} fn
+ */
 let asyncHandler = fn =>
-    (req, res, next) => {
-        Promise.resolve(fn(req, res, next)).catch(next);
-    }
+  (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  }
 
 module.exports = function(checkJwt) {
-    router.use(checkJwt, hasPermission);
+  router.use(checkJwt, hasPermission);
 
-    router.get("/find", asyncHandler(async (req, res) => {
-        //find the profile connected to the email
-        let reqEmail = req.query.e;
-        let resp = { found: false, user: null }; // the response to get request
-        try {
-            let token = await obtainAccessToken("manager");
+   // find the profile connected to the email.
+  router.get("/find", asyncHandler(async (req, res) => {
+    let reqEmail = req.query.e;
+    // the response to get request
+    let resp = { found: false, user: null };
+    try {
+      let token = await obtainAccessToken("manager");
 
-            let { data: users } = await axios.get("https://marine-plastics-coi.auth0.com/api/v2/users-by-email", {
-                params: {
-                    email: reqEmail
-                },
-                headers: {
-                    authorization: `Bearer ${token}`
-                }
-            });
-            //if array is empty no user found
-            if (users.length < 1) {
-                return res.json(resp);
-            }
-
-            resp.found = true;
-            resp.user = users[0];
-
-            //get profile roles
-            let { data: roles } = await axios.get(`https://marine-plastics-coi.auth0.com/api/v2/users/${resp.user.user_id}/roles`, {
-                headers: {
-                    authorization: `Bearer ${token}`
-                }
-            })
-            resp.user.roles = roles;
-            return res.json(resp);
-
-        } catch (err) {
-            let { data: response } = err.response;
-            if (response.statusCode === 400) {
-                return res.status(400).json({ err: response.error });
-            }
-            res.status(500).json({ err: "Something went wrong!" });
+      let { data: users } = await
+        axios.get(
+          "https://marine-plastics-coi.auth0.com/api/v2/users-by-email", {
+        params: {
+          email: reqEmail
+        },
+        headers: {
+          authorization: `Bearer ${token}`
         }
+      });
+      // if array is empty no user found
+      if (users.length < 1) {
+        return res.json(resp);
+      }
 
+      resp.found = true;
+      resp.user = users[0];
+
+      //get profile roles
+      let { data: roles } = await axios.get(
+        `https://marine-plastics-coi.auth0.com/api/v2/users/${resp.user.user_id}/roles`, {
+        headers: {
+          authorization: `Bearer ${token}`
+        }
+      })
+      resp.user.roles = roles;
+      return res.json(resp);
+    } catch (err) {
+      let { data: response } = err.response;
+      if (response.statusCode === 400) {
+        return res.status(400).json({ err: response.error });
+      }
+      res.status(500).json({ err: "Something went wrong!" });
+    }
+  }));
+
+  router.route("/setRole/:id")
+     // set roles for a profile
+    .put(asyncHandler(async (req, res) => {
+      let { id: userID } = req.params;
+
+      try {
+        let token = await obtainAccessToken("manager");
+        await axios.post(
+          `https://marine-plastics-coi.auth0.com/api/v2/users/${userID}/roles`,
+          {
+          roles: ['rol_TeEKH4d1DDLAbCVT']
+        }, {
+          headers: {
+            'cache-control': 'no-cache',
+            Authorization: `Bearer ${token}`,
+            'content-type': 'application/json'
+          }
+        });
+        res.json({ res: "success", role_id: "rol_TeEKH4d1DDLAbCVT" });
+      } catch (err) {
+        req.status(500).json({ err: "Something went wrong!" });
+      }
+    }))
+    // remove role from profile
+    .delete(asyncHandler(async (req, res) => {
+      let { id: userID } = req.params;
+      let token = await obtainAccessToken("manager");
+      try {
+        await axios.delete(
+          `https://marine-plastics-coi.auth0.com/api/v2/users/${userID}/roles`,
+          {
+          data: {
+            roles: ['rol_TeEKH4d1DDLAbCVT']
+          },
+          headers: {
+            'cache-control': 'no-cache',
+            Authorization: `Bearer ${token}`,
+            'content-type': 'application/json'
+          }
+        });
+        res.json({ res: "success" });
+      } catch (err) {
+        req.status(500).json({ err: "Something went wrong!" });
+      }
     }));
 
-
-    router.route("/setRole/:id")
-        .put(asyncHandler(async (req, res) => {
-            //set roles for a profile
-            let { id: userID } = req.params;
-
-            try {
-                let token = await obtainAccessToken("manager");
-                await axios.post(`https://marine-plastics-coi.auth0.com/api/v2/users/${userID}/roles`, {
-                    roles: ['rol_TeEKH4d1DDLAbCVT']
-                }, {
-                    headers: {
-                        'cache-control': 'no-cache',
-                        Authorization: `Bearer ${token}`,
-                        'content-type': 'application/json'
-                    }
-                });
-                res.json({ res: "success", role_id: "rol_TeEKH4d1DDLAbCVT" });
-
-            } catch (err) {
-                req.status(500).json({ err: "Something went wrong!" });
-            }
-        }))
-        .delete(asyncHandler(async (req, res) => {
-            let { id: userID } = req.params;
-            //remove role from profile
-            let token = await obtainAccessToken("manager");
-            try {
-                await axios.delete(`https://marine-plastics-coi.auth0.com/api/v2/users/${userID}/roles`, {
-                    data: {
-                        roles: ['rol_TeEKH4d1DDLAbCVT']
-                    },
-                    headers: {
-                        'cache-control': 'no-cache',
-                        Authorization: `Bearer ${token}`,
-                        'content-type': 'application/json'
-                    }
-                });
-                res.json({ res: "success" });
-            } catch (err) {
-                req.status(500).json({ err: "Something went wrong!" });
-            }
-        }));
-
-
-    router.get("/getAdmins", asyncHandler(async (req, res) => {
-        // get all admins
-        try {
-            let token = await obtainAccessToken("manager");
-            let { data } = await axios.get("https://marine-plastics-coi.auth0.com/api/v2/roles/rol_TeEKH4d1DDLAbCVT/users", {
-                headers: {
-                    authorization: `Bearer ${token}`
-                }
-            });
-            res.json(data);
-        } catch (err) {
-            req.status(500).json({ err: "Something went wrong!" });
+  // gets all admins
+  router.get("/getAdmins", asyncHandler(async (req, res) => {
+    try {
+      let token = await obtainAccessToken("manager");
+      let { data } = await axios.get(
+        "https://marine-plastics-coi.auth0.com/api/v2/roles/rol_TeEKH4d1DDLAbCVT/users", {
+        headers: {
+          authorization: `Bearer ${token}`
         }
-    }));
+      });
+      res.json(data);
+    } catch (err) {
+      req.status(500).json({ err: "Something went wrong!" });
+    }
+  }));
 
-    return router;
+  return router;
 }
--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+/**
+ * server.js
+ * Sets up the different software parts of the app, including the database and
+ * linking.
+ */
 'use strict'
 
 const express = require('express');
@@ -6,42 +11,42 @@ require('dotenv').config();
 const path = require('path');
 const app = express();
 
-const { router: dataEntryRouter, verifySurveyJWT } = require('./routes/dataEntry');
+const { router: dataEntryRouter, verifySurveyJWT } =
+  require('./routes/dataEntry');
 const auth0Route = require('./routes/auth0');
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
 
 const jwtUser = jwt({
-    secret: jwksRsa.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: 'https://marine-plastics-coi.auth0.com/.well-known/jwks.json'
-    }),
-    audience: 'https://user-auth.com',
-    issuer: 'https://marine-plastics-coi.auth0.com/',
-    algorithms: ['RS256']
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://marine-plastics-coi.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'https://user-auth.com',
+  issuer: 'https://marine-plastics-coi.auth0.com/',
+  algorithms: ['RS256']
 });
-
 
 verifySurveyJWT(jwtUser);   
 
-//set our port to either a predetermined port number if you have set it up, or 3001
+// sets port to either a predetermined port number if it is set up, or 3001
 app.use(express.static(path.join(__dirname, 'client/build')));
 let reactPath = path.join(__dirname, "/client/build/index.html");
 
 const port = process.env.PORT || 3001;
 
-//now we should configure the APi to use bodyParser and look for JSON data in the body
+// configures the APi to use bodyParser and look for JSON data in the body
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(function(err, req, res, next) {
-    console.error(err.stack)
-    res.status(500).send('Something broke!')
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
 });
 
-//Use our router configuration when we call /api
+// use our router configuration when we call /api
 app.use('/beaches', dataEntryRouter);
 app.use('/auth', auth0Route(jwtUser));
 
@@ -55,9 +60,7 @@ app.get('/pdfs/ShorelinePlasticMonitoringFieldGuide.pdf',
 
 app.get('*', (req, res) => res.sendFile(reactPath));
 
-
-
-//starts the server and listens for requests
+// starts the server and listens for requests
 app.listen(port, function() {
-    console.log(`api running on port ${port}`);
+  console.log(`api running on port ${port}`);
 });
