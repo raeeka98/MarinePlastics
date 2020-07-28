b7ed0477a8538fda55baa7881aa5c88bb6315186
Sean Gibson <stgibson@ucsc.edu>
2020-07-28 13:15:35 -0700

    Added test e var for reason, majorUse, and st. Modified client/src/Tests/surveyEntryEdit-test-data.js.

--- client/src/Tests/surveyEntryEdit-test-data.js
+++ client/src/Tests/surveyEntryEdit-test-data.js
@@ -3,3 +3,66 @@
  * Contains sample state variables for automated testing. Used in
  * surveyEntryEdit.test.js.
  */
+
+// for testing editSurveyCheckBoxes(e) on clicking reason.prox checkbox
+var editSurveyCheckBoxesEReasonProx = {
+  target: {
+    name = "reason.prox"
+  }
+};
+
+// for testing editSurveyCheckBoxes(e) on clicking reason.debris checkbox
+var editSurveyCheckBoxesEReasonDebris = {
+  target: {
+    name = "reason.debris"
+  }
+};
+
+// for testing editSurveyCheckBoxes(e) on clicking majorUse.rec checkbox
+var editSurveyCheckBoxesEMajorUseRec = {
+  target: {
+    name = "majorUse.rec"
+  }
+};
+
+// for testing editSurveyCheckBoxes(e) on clicking majorUse.com checkbox
+var editSurveyCheckBoxesEMajorUseCom = {
+  target: {
+    name = "majorUse.com"
+  }
+};
+
+// for testing editSurveyCheckBoxes(e) on clicking majorUse.rem checkbox
+var editSurveyCheckBoxesEMajorUseRem = {
+  target: {
+    name = "majorUse.rem"
+  }
+};
+
+// for testing editSurveyCheckBoxes(e) on clicking st.s checkbox
+var editSurveyCheckBoxesEStS = {
+  target: {
+    name = "st.s"
+  }
+};
+
+// for testing editSurveyCheckBoxes(e) on clicking st.p checkbox
+var editSurveyCheckBoxesEStP = {
+  target: {
+    name = "st.p"
+  }
+};
+
+// for testing editSurveyCheckBoxes(e) on clicking st.rr checkbox
+var editSurveyCheckBoxesEStRr = {
+  target: {
+    name = "st.rr"
+  }
+};
+
+// for testing editSurveyCheckBoxes(e) on clicking st.sea checkbox
+var editSurveyCheckBoxesEStSea = {
+  target: {
+    name = "st.sea"
+  }
+};
