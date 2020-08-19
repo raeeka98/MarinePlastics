885581d91e6b34c40270f50df2a14ca93e1b42be
Sean Gibson <stgibson@ucsc.edu>
2020-08-19 16:37:31 -0700

    Removed of commented out obsolete code. Modified render() in client/src/SurveyEntry/SurveyEntry.jsx.

--- client/src/SurveyEntry/SurveyEntry.jsx
+++ client/src/SurveyEntry/SurveyEntry.jsx
@@ -717,29 +717,12 @@ class SurveyEntry extends Component {
                 this.state.surveyData.majorUse ?
                   <p><strong>Major Use: </strong>
                     {this.getCheckBoxData("majorUse")}
-                    {
-                      /*
-                      this.state.surveyData.majorUse.rec ? "Recreation" :
-                      this.state.surveyData.majorUse.com ? "Commercial" :
-                      this.state.surveyData.majorUse.rem ? "Remote/Unused" :
-                      this.state.surveyData.majorUse.other
-                      */
-                    }
                   </p> : null
               }
               {
                 this.state.surveyData.st ?
                   <p><strong>Substrate Type: </strong>
                     {this.getCheckBoxData("st")}
-                    {
-                      /*
-                      this.state.surveyData.st.s ? "Sand" :
-                      this.state.surveyData.st.p ? "Pebbles" :
-                      this.state.surveyData.st.rr ? "Rip Rap" :
-                      this.state.surveyData.st.sea ? "Seaweed" :
-                      this.state.surveyData.st.other
-                      */
-                    }
                   </p>
                   : null
               }
